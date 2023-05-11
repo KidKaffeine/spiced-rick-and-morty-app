1e044a2b4556090497d81ec3efa457475eedc17e
VANESSA MARQUES <vanessa.m.a1989@gmail.com>
2023-05-11 12:10:38 +0200

    added dynamic htlm to document and added respective classes

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
-
-
-
+import { createCharacterCard } from "./components/card/card.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
@@ -17,3 +14,42 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// API Call
+
+// export async function fetchData () {
+//   try {
+//        const apiUrl = "https://rickandmortyapi.com/api/character"
+//        const response = await fetch(apiUrl); 
+//        const data = await response.json();
+//        let characters = data.results
+
+      
+//       characters.forEach(character => {
+
+
+
+//           const characterName  = character.name;
+//           console.log(characterName)
+
+//           const imgSrc = character.image;
+
+//           const characterStatus = character.status;
+
+//           const characterType = character.type;
+
+//           createCharacterCard(characterName, characterStatus, characterType, imgSrc)
+//         });
+
+//       // console.log(data.info.next) 
+//       // console.log(data.info.prev)
+//   } 
+
+//   catch {
+//       console.log('This is an error. Yay.')
+//   }
+// }
+
+// fetchData();
+createCharacterCard()
+
