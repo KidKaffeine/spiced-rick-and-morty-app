957e26e3c3a88c21807b4a6771e9f867f53fbd94
VANESSA MARQUES <vanessa.m.a1989@gmail.com>
2023-05-11 10:37:21 +0200

    created initial element structure for charecter card

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,29 @@
-export function createCharacterCard() {}
+export function createCharacterCard(characterData) {
+
+    const cardContainer = document.createElement('ul');
+    const card = document.createElement.apply('li'); 
+    
+    const cardImageContainer = document.createElement('div');
+    const cardImage = document.createElement('img'); 
+    
+    const cardImageGradient = document.createElement('div');
+    const cardContent = document.createElement('div'); 
+    
+    const cardTitle = document.createElement('h2');
+    
+    const cardInfo = document.createElement('dl');
+    
+    const cardInfoTitleStatus = document.createElement('dt');
+    const cardInfoDescriptionStatus = document.createElement('dd');
+    
+    const cardInfoTitleType = document.createElement('dt');
+    const cardInfoDescriptionType = document.createElement('dd');
+    
+    const cardInfoTitleOccurences = document.createElement('dt');
+    const cardInfoDescriptionOccurences = document.createElement('dd');
+    
+    
+}
+
+
+
